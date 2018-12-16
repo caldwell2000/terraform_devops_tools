1375ddf569de328c0be896b24f4d7837de4110c5
Rich Caldwell <richcaldwell2000@yahoo.com>
2018-12-15 20:00:24 -0600

    EFS and EC2 Role 2

--- scripts/jenkins-master.sh
+++ scripts/jenkins-master.sh
@@ -8,14 +8,14 @@ echo /dev/nvme1n1  /data1 xfs defaults,nofail 0 2 >> /etc/fstab
 
 # Patch OS
 echo "Install OS Patches"
-sudo yum update -y
+yum update -y
 
 echo "Install common tools"
-sudo yum install -y tcpdump telnet bind-utils wget zip unzip 
+yum install -y tcpdump telnet bind-utils wget zip unzip 
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-sudo yum install -y pygpgme yum-utils 
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+yum install -y pygpgme yum-utils 
 
 # Mount EFS Filesystem
 MP="/JENKINS_HOME"
@@ -23,7 +23,7 @@ mkdir –p $MP
 EFS_FSID=`/usr/local/bin/aws --region us-east-2 --output text efs describe-file-systems |awk '{print $5}'`
 AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/'`
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $AZ.$EFS_FSID.efs.$REGION.amazonaws.com:/ $MP
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $AZ.$EFS_FSID.efs.$REGION.amazonaws.com:/ $MP
 
 echo "Install Jenkins stable release"
 yum install -y nfs-utils
@@ -50,9 +50,9 @@ sdk install groovy
 groovy -version
 
 echo "Install Docker engine"
-sudo yum install -y yum-utils
-sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
-sudo yum install -y docker
+yum install -y yum-utils
+yum-config-manager --enable rhui-REGION-rhel-server-extras
+yum install -y docker
 usermod -aG docker ec2-user
 service docker start
 
